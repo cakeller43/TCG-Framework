00f1149d682adf3d5f688dbac49ac5f6817576ba
Corey Keller <cakeller43@gmail.com>
2016-01-24 11:49:41 -0600

    RabbitMQ Commit
    Install Packages.RabbitMQ and JSON.
    Add IGameEvent.
    Add TestEvent.
    Add CardSystem project.
    Add EventConsumer.

---
+++ CCG/Assets/Solution/CCG/CCG/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CCG.Core;
+using CCG.EventTypes;
+
+namespace CCG
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var handler = new EventProducer();
+            
+            handler.PublishEvent(new TestEvent { RoutingKey = "key1"});
+            handler.PublishEvent(new TestEvent { RoutingKey = "key2" });
+            handler.PublishEvent(new TestEvent { RoutingKey = "key3" });
+            handler.PublishEvent(new TestEvent { RoutingKey = "key4" });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CCG/Assets/Solution/CCG/CardSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CardSystem
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] routingKeys = { "key1", "key2", "key3" };
+            var consumer = new EventConsumer(routingKeys);
+
+            Console.ReadLine();
+        }
+    }
+}
